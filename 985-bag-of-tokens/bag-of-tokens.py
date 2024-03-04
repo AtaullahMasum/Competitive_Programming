274fe15f3fbca8ff86175a3c67ca247cd349c0a3
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-04 07:35:11 +0600

    Time: 52 ms (65.45%) | Memory: 16.7 MB (64.63%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.py
@@ -0,0 +1,22 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        score = 0
+        left = 0
+        right = len(tokens) - 1
+        while left <= right:
+            if power >= tokens[left] :
+                power -= tokens[left]
+                score += 1
+                left += 1
+            else:
+                if power + tokens[right] > power and score != 0 and right != left:
+                    power += tokens[right]
+                    right -= 1
+                    score -= 1
+                else:
+                    break
+        return score
+
+            
+        
