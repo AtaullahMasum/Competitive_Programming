930d4908a5e66eb482ec79694f61f80709a8e2af
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-05 16:22:20 +0600

    Time: 92 ms (77.49%) | Memory: 17.9 MB (72.55%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,33 +1,22 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        
-        graph = defaultdict(list)
-        for u, v, w in flights:
-            graph[u].append((v, w))
-        
-        pq = [(0, src, 0)]  # (cost, node, stops)
-        visited = [float('inf')] * n
-        
-        while pq:
-            cost, node, stops = heapq.heappop(pq)
-            if node == dst:
-                return cost
-            if stops > k or stops >=visited[node] :
+        graph = [[] for _ in range(n)]
+        for u,v,price in flights:
+            graph[u].append((v,price))
+        dist = [float('inf')] *n
+        dist[src] = 0
+        queue = [(0,src,0)]
+        while queue:
+            stops, node, cost = queue.pop(0)
+            if stops > k:
                 continue
-            visited[node] = stops
-            for neighbor, neighbor_cost in graph[node]:
-                heapq.heappush(pq, (cost + neighbor_cost, neighbor, stops + 1))
-        
-        return -1
-        INF = float('inf')
-        dp = [[INF] * n for _ in range(k + 2)]  # K stops + 1 and destination
-        dp[0][src] = 0
+            for adjacent, weight in graph[node]:
+                if weight + cost < dist[adjacent] and stops <= k:
+                    dist[adjacent] = weight + cost
+                    queue.append((stops+1, adjacent, dist[adjacent]))
+        if dist[dst] == float('inf'):
+            return -1
+        return dist[dst]
 
-        for i in range(1, k + 2):
-            dp[i][src] = 0  # Base case: no cost to reach source
 
-        for i in range(1, k + 2):
-            for u, v, w in flights:
-                dp[i][v] = min(dp[i][v], dp[i - 1][u] + w)
 
-        return dp[k + 1][dst] if dp[k + 1][dst] != INF else -1
