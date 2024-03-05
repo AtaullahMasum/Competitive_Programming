cecadc1a02c818834a96bf4f706ad75c517f315c
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-05 10:28:16 +0600

    Time: 408 ms (76.65%) | Memory: 17.1 MB (75.57%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -7,6 +7,24 @@ class Solution:
         dist = [[float('inf')]*cols for _ in range(rows)]
         direction = [(1,0),(1,-1),(1,1),(-1,0),(-1,1),(-1,-1),(0,-1),(0,1)]
         dist[0][0] = 1
+        queue = [(1,0,0)]
+       
+        while queue:
+            cost , u, v = queue.pop(0)
+            if u == rows-1 and v == cols-1:
+                return dist[u][v]
+            for nr, nc in direction:
+                dr, dc = nr+u, nc + v
+                if 0<=dr < rows and 0<=dc<cols and grid[dr][dc]==0 and dist[dr][dc]>1+dist[u][v]:
+                    dist[dr][dc] = 1 + dist[u][v]
+                    queue.append((dist[dr][dc],dr,dc))
+        return -1
+        if grid[0][0] != 0:
+            return -1
+        rows, cols = len(grid), len(grid[0])
+        dist = [[float('inf')]*cols for _ in range(rows)]
+        direction = [(1,0),(1,-1),(1,1),(-1,0),(-1,1),(-1,-1),(0,-1),(0,1)]
+        dist[0][0] = 1
         heap = []
         heapq.heappush(heap,(1,0,0))
         while heap:
