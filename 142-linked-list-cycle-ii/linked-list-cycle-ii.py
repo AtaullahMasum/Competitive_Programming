cfa0ec4636a177885810bb56bf3269808015e1da
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-06 10:11:51 +0600

    Time: 51 ms (14.96%) | Memory: 19 MB (84.43%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -6,21 +6,33 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+       # Check if the linked list is empty or has only one node
         if not head or not head.next:
             return None
+        
+        # Initialize slow and fast pointers
         slow = head
         fast = head
+        
+        # Move slow and fast pointers until they meet inside the cycle
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+            
+            # If slow and fast pointers meet, it indicates the presence of a cycle
             if slow == fast:
                 break
-        if slow == fast:
-            slow = head
-            while slow != fast:
-                slow = slow.next
-                fast = fast.next
-            return slow
         else:
-            return None
+            return None  # No cycle found
+        
+        # Move slow pointer back to the head
+        slow = head
+        
+        # Move slow and fast pointers until they meet at the start of the cycle
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        
+        # Return the node where the cycle starts
+        return slow
         
