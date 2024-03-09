c8c4a024de524d91b63aac671e9efc056131c555
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-09 12:00:01 +0600

    Time: 399 ms (37.16%) | Memory: 36.4 MB (90.28%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -0,0 +1,40 @@
+class DisjointSet:
+  def __init__(self, V):
+    self.parent = [i for i in range(V+1)]
+    self.rank = [0]*(V+1)
+  def findUPa(self, node):
+    if node == self.parent[node]:
+      return node
+    self.parent[node] = self.findUPa(self.parent[node])
+    return self.parent[node]
+  def unionByRank(self, u, v):
+    ult_pa_u = self.findUPa(u) # ultimate parent of u
+    ult_pa_v = self.findUPa(v) # ultimate parent of v
+    if ult_pa_u == ult_pa_v:
+      return
+    if self.rank[ult_pa_u] < self.rank[ult_pa_v]:
+      self.parent[ult_pa_u] = ult_pa_v
+    elif self.rank[ult_pa_v] < self.rank[ult_pa_u]:
+      self.parent[ult_pa_v] = ult_pa_u
+    else:
+      self.parent[ult_pa_v] =  ult_pa_u
+      self.rank[ult_pa_u] += 1
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        ds = DisjointSet(n)
+        extraedges = 0
+        for u, v in connections:
+            if ds.findUPa(u)==ds.findUPa(v):
+                extraedges += 1
+            else:
+                ds.unionByRank(u,v)
+        numberofcomponent = 0
+        for i in range(n):
+            if ds.findUPa(i) == i:
+                numberofcomponent += 1
+        ans = numberofcomponent - 1
+        if extraedges >= ans:
+            return ans
+        return -1
+
+        
