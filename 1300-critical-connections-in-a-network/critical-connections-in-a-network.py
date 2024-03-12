457f0b61ec421874d3c106624629b9e2f6c767c8
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-12 15:15:54 +0600

    Update critical-connections-in-a-network.py

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.py
@@ -1,6 +1,21 @@
 
 class Solution:
-    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+  def  __init__(self):
+    self.time = 0
+  def dfs(self, node, disc, low, parent,bridges, adj ):
+    disc[node] = self.time
+    low[node] = self.time
+    self.time += 1
+    for adjacent_node in adj[node]:
+      if disc[adjacent_node] == -1:
+        parent[adjacent_node] = node 
+        self.dfs(adjacent_node,disc, low, parent,bridges, adj)
+        low[node] = min(low[node], low[adjacent_node])
+        if low[adjacent_node] > disc[node]:
+          bridges.append((node, adjacent_node))
+      elif adjacent_node != parent[node]: #if child to parent then ignore
+        low[node] = min(low[node], disc[adjacent_node])
+  def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
         graph = [[]for _ in range(n)]
         for u, v in connections:
             graph[u].append(v)
@@ -10,22 +25,7 @@ class Solution:
         disc = [-1]*n
         low = [-1]*n
         bridges = []
-        def dfs(node):
-            nonlocal time
-            disc[node] = time
-            low[node] = time
-            time += 1
-            for adjacent_node in graph[node]:
-                if disc[adjacent_node] == -1:
-                    parent[adjacent_node] = node
-                    dfs(adjacent_node)
-                    low[node] = min(low[node], low[adjacent_node])
-
-                    if low[adjacent_node] > disc[node]:
-                        bridges.append([node, adjacent_node])
-                elif adjacent_node != parent[node]:
-                    low[node] = min(low[node], disc[adjacent_node])
         for node in range(n):
             if disc[node] == -1:
-                dfs(node)
+                self.dfs(node, disc, low, parent,bridges, graph)
         return bridges
