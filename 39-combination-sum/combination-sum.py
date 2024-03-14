6bef3f4db73bcb0a122ae904ef20660e10904fbb
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-15 00:33:01 +0600

    Time: 48 ms (79.05%) | Memory: 16.6 MB (49.95%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -3,10 +3,9 @@ class Solution:
         ans = []
         comb = []
         def backtrack(i, target):
-            if target == 0:
-               ans.append(comb.copy())
-               return
             if i == len(candidates):
+                if target == 0:
+                    ans.append(comb.copy())
                 return 
             if candidates[i] <= target:
                 comb.append(candidates[i])
