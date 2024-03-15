ebe08002315a5c07c353bd4fc8ce7ce396fd4be0
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-15 21:26:12 +0600

    Time: 38 ms (66.27%) | Memory: 16.6 MB (91.13%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findSubset(self, index, nums, result, each_result):
+       
+        result.append(each_result.copy())
+        for i in range(index, len(nums)):
+            if i >   index and nums[i]==nums[i-1]:
+                continue
+            each_result.append(nums[i])
+            self.findSubset(i+1, nums, result, each_result)
+            each_result.pop()
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        each_result = []
+        nums.sort()
+        self.findSubset(0,nums, result, each_result)
+        return result
+        
