0844d5ac2daee1168cf78e79af00ca262011f808
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-15 14:20:54 +0600

    Time: 46 ms (89.80%) | Memory: 16.6 MB (80.40%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,6 +1,23 @@
 class Solution:
+    def findCombination(self, index, target, candidates, result, each_result):
+        if target == 0:
+            result.append(each_result.copy())
+            return
+        for i in range(index, len(candidates)):
+            if i > index and candidates[i]== candidates[i-1]:
+                continue
+            if candidates[i] > target:
+                break
+            each_result.append(candidates[i])
+            self.findCombination(i+1, target-candidates[i], candidates, result, each_result)
+            each_result.pop()
    
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        each_result = []
+        candidates.sort()
+        self.findCombination(0, target, candidates, result, each_result)
+        return result
         def backtrack(start, target, path, result):
             if target== 0:
                 result.append(path[:])
