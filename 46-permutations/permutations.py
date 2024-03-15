e9adf6249686e4870c64b451e83638a544692f0e
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-16 00:14:57 +0600

    Time: 38 ms (75.96%) | Memory: 16.6 MB (78.69%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,4 +1,12 @@
 class Solution:
+    def findAllPermutation(self, index, nums, result):
+        if index == len(nums):
+            result.append(nums[:])
+            return 
+        for i in range(index, len(nums)):
+            nums[i], nums[index] = nums[index], nums[i]
+            self.findAllPermutation(index+1, nums, result)
+            nums[i], nums[index] = nums[index], nums[i]
     def findallpermutation(self, freq, nums, result, each_result):
         if len(each_result)==len(nums):
             result.append(each_result[:])
@@ -13,6 +21,11 @@ class Solution:
             
 
     def permute(self, nums: List[int]) -> List[List[int]]:
+        #method 1
+        result = []
+        self.findAllPermutation(0, nums, result)
+        return result
+        #method 2
         n = len(nums)
         freq = [False]*n
         result , each_result = [], []
