ddd859c96692a47eb49c5cc0d4d6d5be0c53881f
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-16 14:18:04 +0600

    Time: 53 ms (60.85%) | Memory: 17.1 MB (47.05%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -34,8 +34,33 @@ class Solution:
                 board[row][col] = 'Q'
                 self.solve(col+1, board, n, result)
                 board[row][col] = '.'
+    def solve1(self,col, board, result, upperDiagonal, lowerDiagonal, leftRow, n):
+        if col == n:
+            result.append(["".join(row) for row in board])
+            return
+        for row in range(n):
+            if leftRow[row] == 0 and upperDiagonal[(n-1)+(col-row)]==0 and lowerDiagonal[row+col]==0:
+                board[row][col] = "Q"
+                leftRow[row] = 1
+                upperDiagonal[(n-1)+(col-row)]= 1
+                lowerDiagonal[row+col] = 1
+                self.solve1(col+1, board, result, upperDiagonal, lowerDiagonal, leftRow, n)
+                board[row][col] = "."
+                leftRow[row] = 0
+                upperDiagonal[(n-1)+(col-row)] = 0
+                lowerDiagonal[row+col] = 0
+
 
     def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        leftRow = [0]*n
+        upperDiagonal = [0]*(2*n -1)
+        lowerDiagonal = [0]*(2*n -1)
+        result = []
+        col = 0
+        self.solve1(col, board, result, upperDiagonal, lowerDiagonal, leftRow, n)
+        return result
+
         board = [['.' for _ in range(n)] for _ in range(n)]
         col = 0
         result = []
